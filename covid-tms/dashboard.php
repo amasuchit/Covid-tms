cb664dda1f0874393aa82b0c62984e2fc624a563
amasuchit <101627284+amasuchit@users.noreply.github.com>
2022-04-10 10:04:57 +0545

    Add files via upload

---
+++ covid-tms/dashboard.php
@@ -0,0 +1,109 @@
+<?php
+session_start();
+include_once('includes/config.php');
+if (strlen($_SESSION['aid']==0)) {
+  header('location:logout.php');
+  } else{
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Covid-Tms | Dashboard</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <?php include_once('includes/sidebar.php');?>
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Topbar -->
+                <?php include_once('includes/topbar.php');?>
+                <!-- End of Topbar -->
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+                        
+                    </div>
+
+                    <!-- Content Row -->
+                    <div class="row">
+
+                        <?php 
+//Total tests
+$query=mysqli_query($con,"select id from tbltestrecord");
+$totaltest=mysqli_num_rows($query);
+
+
+
+?>
+
+                        <!-- Total Tests-->
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-primary shadow h-100 py-2">
+                                <a href="all-test.php">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+
+
+                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                             Total Tests</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $totaltest;?></div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-virus fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                            </div>
+                        </div>
+
+                      
+
+
+                </div>
+             
+
+                
+                <?php include_once('includes/footer2.php');?>
+              
+
+              
+</body>
+
+</html>
+<?php } ?>
---
+++ covid-tms/includes/config.php
@@ -0,0 +1,9 @@
+<?php
+
+//database connection
+$con=mysqli_connect("localhost","root","","covidtmsdb");
+if(mysqli_connect_errno()){
+echo "Connection Fail".mysqli_connect_error();
+}
+
+  ?>
